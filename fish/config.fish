893990de5b454f2c860b5b12ea838d848c6f5042
Filip Andrei-Robert <filandrei4@protonmail.com>
2026-04-03 22:43:06 +0300

    bakudeku slime

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,29 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 
-set browser qutebrowser
+# Init
+zoxide init fish | source
+starship init fish | source
 
+
+
+
+# Alias
+alias cat="bat"
+alias src="source ~/.config/fish/config.fish"
+alias ls="ls --color=auto"
+alias browser="qutebrowser"
+
+
+# =))
 function gpt 
 	if test (count $argv) -lt 1
 		echo "gpt <prompt>"
 		exit 0
 	end
-	$browser "https://chat.openai.com/?prompt=$argv[1]" &> /dev/null &
+	browser "https://chat.openai.com/?prompt=$argv[1]" &> /dev/null &
 end
+
+
 
 end
